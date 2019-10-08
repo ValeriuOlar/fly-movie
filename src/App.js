46393e3391539d67700b181afb250bc1724d0373
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-10-08 21:59:10 +0100

    adding content

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ class App extends Component{
       console.log("This is the Initialiser")
 
       const movies =[
-        {id: 0, title: "", overview: ""},
-        {id: 1, title: "", overview: ""},
-        {id: 2, title: "", overview: ""}
+        {id: 0, title: "Avengers: Infinity War", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est arcu, tempus quis leo ac, commodo hendrerit velit. Maecenas in imperdiet velit. Vivamus ac diam mauris. Donec posuere, neque ut bibendum feugiat, justo quam egestas elit, nec malesuada nisl elit commodo libero. Suspendisse ultricies lorem urna. Aenean arcu purus, pulvinar at tincidunt id, volutpat id quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."},
+        {id: 1, title: "Avengers: Endgame", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est arcu, tempus quis leo ac, commodo hendrerit velit. Maecenas in imperdiet velit. Vivamus ac diam mauris. Donec posuere, neque ut bibendum feugiat, justo quam egestas elit, nec malesuada nisl elit commodo libero. Suspendisse ultricies lorem urna. Aenean arcu purus, pulvinar at tincidunt id, volutpat id quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."},
+        {id: 2, title: "Avengers: Age of Ultron", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est arcu, tempus quis leo ac, commodo hendrerit velit. Maecenas in imperdiet velit. Vivamus ac diam mauris. Donec posuere, neque ut bibendum feugiat, justo quam egestas elit, nec malesuada nisl elit commodo libero. Suspendisse ultricies lorem urna. Aenean arcu purus, pulvinar at tincidunt id, volutpat id quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."},
+        {id: 3, title: "Captain Marvel", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est arcu, tempus quis leo ac, commodo hendrerit velit. Maecenas in imperdiet velit. Vivamus ac diam mauris. Donec posuere, neque ut bibendum feugiat, justo quam egestas elit, nec malesuada nisl elit commodo libero. Suspendisse ultricies lorem urna. Aenean arcu purus, pulvinar at tincidunt id, volutpat id quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."},
+        {id: 4, title: "Captain America: The First Avenger", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est arcu, tempus quis leo ac, commodo hendrerit velit. Maecenas in imperdiet velit. Vivamus ac diam mauris. Donec posuere, neque ut bibendum feugiat, justo quam egestas elit, nec malesuada nisl elit commodo libero. Suspendisse ultricies lorem urna. Aenean arcu purus, pulvinar at tincidunt id, volutpat id quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."}
       ]
 
       this.state = {rows: [
       <p key="1">This is a row</p>,
       <p key="2">This is a row</p>,
-      <p key="3">This is a row</p>
+      <p key="3">This is a row</p>,
+      <p key="4">This is a row</p>,
+      <p key="5">This is a row</p>
 
     ]}
   }
