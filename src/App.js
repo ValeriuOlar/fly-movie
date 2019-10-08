28233df8a216b6e513d7a9a7a10c6587c0978f1f
Valeriu Olariu <15169056+ValeriuOlar@users.noreply.github.com>
2019-10-08 08:56:59 +0100

    initialiser

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 import './App.css';
 
 class App extends Component{
+    constructor(props) {
+      super(props)
+      console.log("This is the Initialiser")
+
+      this.state = {rows: <p>This is a row</p>}
+    }
+
+
   render() {
     return(
       <div className="App">
@@ -21,7 +29,10 @@ class App extends Component{
       <input style={{
         fontSize: 24,
         display: 'block',
-        width: "99%"
+        width: "99%",
+        paddingTop: 8,
+        paddingBottom: 8,
+        paddingLeft: 16
       }}
       placeholder="Enter search term"/>
 
